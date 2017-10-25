05c286add46dee5a2cf48501b9e2b2f0fdb18d6f
TRACLabs <pushpaparam02@gmail.com>
2017-10-25 13:27:19 -0500

    added second comment and committed

--- JenkinsGitBuild/PrintDemo1.java
+++ JenkinsGitBuild/PrintDemo1.java
@@ -5,7 +5,8 @@ public class PrintDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This a test build for Jenkins and Git ");
+		System.out.println("This a test build for Jenkins and Git  ");
+//		added a comment
 	}
 
 }
