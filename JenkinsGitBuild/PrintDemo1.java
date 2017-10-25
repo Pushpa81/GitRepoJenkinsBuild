adcd8ca0d176f780b5d740eef2e2e1082efba6a8
TRACLabs <pushpaparam02@gmail.com>
2017-10-25 13:25:56 -0500

    first commit after a change

---
+++ JenkinsGitBuild/PrintDemo1.java
@@ -0,0 +1,11 @@
+package JenkinsGitPkg;
+
+public class PrintDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This a test build for Jenkins and Git ");
+	}
+
+}
---
+++ JenkinsGitBuild/src/JenkinsGitPkg/PrintDemo1.java
@@ -0,0 +1,11 @@
+package JenkinsGitPkg;
+
+public class PrintDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This Jenkins build using Git");
+	}
+
+}
